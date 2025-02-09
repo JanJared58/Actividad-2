915d4fd935af547818a2250b2b64dff205dd3043
JanJared58 <janjaredrmz32@gmail.com>
2025-02-09 01:18:01 -0600

    Ej2

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -1,3 +1,13 @@
-num = int(input("Ingrese un numero de 1 al 20"))
+num = int(input("Ingrese un numero de 1 al 20: "))
 
+while num<1 or num>20:
+    num = int(input("Ingrese un numero de 1 al 20: "))
 
+result = 1
+n=num
+
+while n > 0:
+    result = result * n
+    n = n - 1
+
+print(f"El factorial es: {result}")
